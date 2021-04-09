c34c0a0efcdf93ceee6492c50ba305e2c8295a38
lwod <lwodcurrentmail@gmail.com>
2021-04-09 10:57:35 +0300

    create some cache-storage with label-name for the static assets

--- sw.js
+++ sw.js
@@ -1,5 +1,17 @@
-self.addEventListener('install', ()=>{
-	console.log('sw install')
+const staticCacheName = 's-app-v1'
+
+const assetUrls = [
+	'index.html',
+	'/js/app.js',
+	'/css/styles.css'
+]
+
+self.addEventListener('install', (event)=>{
+	
+	event.waitUntil(
+		caches.open(staticCacheName).then(cache => cache.addAll(assetUrls))
+	)
+	
 })
 
 self.addEventListener('activate', ()=>{
